11a71841c45b94d4115abfe2a04d89e41f8c000d
Kitty <o0zlxl0o@naver.com>
2024-12-05 16:15:31 +0900

    24.12.05(목)
    목요일 수업완료

--- 002. HTML학습/07.ShoppingMall_PJ/js/main1.js
+++ 002. HTML학습/07.ShoppingMall_PJ/js/main1.js
@@ -4,53 +4,110 @@
 import myFn from "./my_function.js";
 
 /***************************************************** 
- 
     [ 슬라이드 이동 기능정의 ]
     1. 이벤트 종류: click
     2. 이벤트 대상: 이동버튼(.abtn)
     3. 변경 대상: 슬라이드 박스(.slide)
     4. 기능 설계:
+
         (1) 오른쪽 버튼 클릭시 다음 슬라이드가
-            나타나도록 슬라이드 박스의 left값을
-            -100%로 변경시킨다.
-            -> 슬라이드 이동후!!! 
-            바깥에 나가있는 첫번째 슬라이드
-            li를 잘라서 맨뒤로 보낸다!
-            동시에 left값을 0으로 변경한다!
+            나타나도록 슬라이드 박스의 translate값을
+            -100% 단위로 값을 증가하여 변경시킨다.
 
-        (2) 왼쪽버튼 클릭시 이전 슬라이드가
-            나타나도록 하기위해 우선 맨뒤 li를
-            맨앞으로 이동하고 동시에 left값을
-            -100%로 변경한다.
-            그 후 left값을 0으로 애니메이션하여
-            슬라이드가 왼쪽에서 들어온다.
+        (2) 왼쪽 버튼 클릭시 이전 슬라이드가
+            나타나도록 슬라이드 박스의 translate값을
+            -100% 단위로 값을 감소하여 변경시킨다.
 
-        (3) 공통기능: 슬라이드 위치표시 블릿
+        (3) 이동 중 첫번째 슬라이드는 왼쪽이동버튼 숨기기
+            마지막 슬라이드는 오른쪽이동버튼 숨기기함!
+
+        (4) 공통기능: 슬라이드 위치표시 블릿
             - 블릿 대상: .indic li
             - 변경 내용: 슬라이드 순번과 같은 순번의
             li에 클래스 "on"주기(나머진 빼기->초기화!)
 
 *****************************************************/
 
-// 전역변수구역 //////////
-/* 
-    (참고: JS에서 이름짓는 일반규칙)
-    1. 변수/함수 : 캐믈케이스(첫단어소문자 뒷단어 대문자시작)
-    2. 생성자함수/클래스 : 파스칼케이스(모든첫글자 대문자)
-    3. 상수 : 모든글자 대문자(연결은 언더스코어-스네이크 케이스)
-*/
-
-// 초기 테스트
-// 오른쪽 버튼 클릭시 ul.slide의 css를 변경하여 왼쪽으로 이동
-// 이벤트 대상 : .ab2
-// 변경 대상 : .slide
-const slide = myFn.qs('.slide');
-//순번변수
+// [전역변수구역] ///////////////
+// (1) 순번변수
 let seqNum = 0;
-myFn.qs('.ab2').onclick = ()=>{
-    // 순번1증가
+////////////////////////////////
+
+// 1. 대상선정 //////////////////
+// (1) 전체 슬라이드 박스 : .slide-box
+const slideBox = myFn.qs(".slide-box");
+// 전체 박스 하위의 요소로 상대적으로 잡아준다!
+// 이유는 다른 슬라이드 박스를 카피하여 만든경우
+// 동일한 기능이 될 수 있게 해준다!
+
+// (2) 이벤트대상: .abtn
+const abtn = myFn.qsaEl(slideBox, ".abtn");
+// 처음에 첫번째 버튼 숨기기
+abtn[0].style.display = "none";
+
+// (3) 변경대상: .slide
+const slide = myFn.qsEl(slideBox, ".slide");
+// 슬라이드 개수 변수할당!
+// 보통 변경없이 사용하는 변수는 상수라고 하고
+// 상수는 보통 대문자로 쓰고 스네이크 케이스 사용함!
+const SLIDE_CNT = myFn.qsaEl(slide, "li").length;
+console.log("슬라이드개수:", SLIDE_CNT);
+
+// console.log("대상:",slideBox,abtn,slide);
+
+// 2. 이벤트 설정하기 ////////////////////
+abtn.forEach((el) => {
+  // console.log('요소:',el);
+  // 각 요소에 이벤트 설정하기
+  // click이벤트를 addEventListener로 설정!
+  myFn.addEvt(el, "click", goSlide);
+}); //////////// forEach ////////////////
+
+// 3. 함수만들기 /////////////////////
+function goSlide() {
+  // 1. 함수호출확인
+  // console.log('나함수!',this);
+
+  // 2. 오른쪽버튼여부 확인
+  let isRight = this.classList.contains("ab2");
+  // classList.contains(클래스명) -> 클래스있으면 true
+  console.log("나함수!", isRight);
+
+  // 3. 분기하여 슬라이드순번 변수 증감하기
+  // (1) 오른쪽일때 증가
+  if (isRight) {
     seqNum++;
-    // 슬라이드 css변경
-    slide.style.transform = (seqNum*-100)+'%';
-    slide.style.transition = '.4s ease-in-out';
-};
+    // 한계값 설정 :
+    // -> 마지막 슬라이드 순번보다 크면 마지막번호고정
+    // 위에서 슬라이드개수는 상수 SLIDE_CNT에 있음!
+    if (seqNum > SLIDE_CNT - 1) seqNum = SLIDE_CNT - 1;
+  } /// if ///
+  // (2) 왼쪽일때 감소
+  else {
+    seqNum--;
+    // 한계값설정 :
+    // -> 0보다 작아지면 0으로 고정
+    if (seqNum < 0) seqNum = 0;
+  } /// else ///
+
+  console.log("슬순번:", seqNum);
+
+  // 4. 슬라이드 CSS변경하여 슬라이드 이동하기
+  slide.style.translate = seqNum * -100 + "%";
+  slide.style.transition = ".4s ease-in-out";
+
+  // 5. 순번이 끝번호일때 버튼 숨기기 보이기 처리
+  if (seqNum === 0) {
+    // 처음슬라이드 왼쪽버튼 숨기기
+    abtn[0].style.display = "none";
+  } /// if ///
+  else if (seqNum === SLIDE_CNT - 1) {
+    // 마지막슬라이드 오른쪽버튼 숨기기
+    abtn[1].style.display = "none";
+  } // else if ///
+  else {
+    // 기타경우 모두보이기
+    abtn[0].style.display = "block";
+    abtn[1].style.display = "block";
+  } /// else ///
+} ////////// goSlide함수 /////////////
---
+++ 004.JS학습/01.JS실험실/js/show_letter.js
@@ -0,0 +1,72 @@
+// JS실험실 : 01. 글자등장1 JS - show_letter.js
+
+// 나의 하뭇 가져오기 //
+import myFn from "./my_function.js";
+
+// 1. 요구사항 분석
+// - 글자를 박스에 넣고 하나씩 일어나면서 등장
+
+// 2. 대상선정
+// 글자를 넣을 박스(3개임)
+const stage = myFn.qsa(".stage-letters");
+
+console.log("대상:", stage);
+
+// 3. 변경내용 적용하기
+// - 박스에 글자를 span으로 싸서 하나씩 모두넣기
+// - 스페이스 문자는 b태그로 변환한다!
+
+// 글자내용 변수
+const myText = [
+  "너의 췌장을 먹고싶어!! 🐷",
+  "추락하는 것은 날개가 있다 🕊",
+  "뻐꾸기 둥지 위로 날아 ༼ つ ◕_◕ ༽つ",
+];
+
+// 배열만큼 돌면서 셋팅하기 : forEach() 메서드
+myText.forEach((v, i) => {
+  // v - 배열값, i - 순번
+  console.log(v, i);
+  // 한글자씩 잘라서 순회하는 제어문은? for of
+  let result = ""; // 결과 변수
+  let dTime = 0; // 지연시간 변수
+  for (let x of v) {
+    // v - 배열값
+    // console.log(x);
+
+    // 스페이스 값일때 b태그 넣기
+    if (x == " ") result += `<b></b>`;
+    // 문자 값이면 span으로 싸기 + 트랜지션 지연시간
+    // 지연 시간은 0.08초에 0,1,2,3,...을 순서대로 곱한다
+    else {
+      result += `
+    <span
+    style="transition-delay:${0.08 * dTime}s">
+    ${x}</span>
+    `;
+      // 다음 순회시 dTime 1씩 증가하기 - 문자일때만
+      console.log("지연시간 곱할수:", dTime);
+      dTime++;
+    }
+  } //// for of ////
+
+  // 4. 결과 출력 : stage는 html컬렉션이다!
+  // -> .stage-letters 3개가 담겨있음
+  // -> 순번은 배열의 순번과 같은 순번에 html넣기!
+  stage[i].innerHTML = result;
+
+  // 5. 글자박스에 클래스 style1, style2, style3 넣기
+  stage[i].classList.add("style" + (i + 1));
+}); // forEach //
+
+const startTime = [2000, 3500, 5000];
+
+// 6. 일정시간 후 stage에 클래스 on 넣어서 글자 등장하기
+stage.forEach((el, idx) => {
+  // el - 출력할 요소, idx - 요소순번
+  console.log("요소:", el, "/ 순번", idx);
+  // 타임아웃 셋팅하기
+  setTimeout(() => {
+    el.classList.add("on");
+  }, startTime[idx]);
+});
